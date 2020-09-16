1df196beb787d31e77f210f2da60a3836e6d6d72
Jimmy Lord <jimmylord@gmail.com>
2020-09-16 12:27:54 -0400

    Added GameCore and Game classes.

--- Framework/Source/GL/MyGLContext.cpp
+++ Framework/Source/GL/MyGLContext.cpp
@@ -62,8 +62,8 @@ bool MyGLContext::AcquireWGLExtensions(HINSTANCE hInstance)
     pfd.nVersion = 1;
     pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
     pfd.iPixelType = PFD_TYPE_RGBA;
-    pfd.cColorBits = 32;
-    pfd.cAlphaBits = 0;
+    pfd.cColorBits = 24;
+    pfd.cAlphaBits = 8;
     pfd.cDepthBits = 24;
 
     GLuint pixelFormat = ChoosePixelFormat( hDeviceContext, &pfd );
